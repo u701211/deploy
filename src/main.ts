4c2d88aedd6889e3360f1b5adc96b09023688003
takahiro tanaka <u701211.job@gmail.com>
2023-02-15 14:44:48 +0900

    commit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="app-release.apk" download>Android APKをダウンロードする (app-release.apk)</a>
-    <a href="app-debug.apk" download>Android APKをダウンロードする (app-debug.apk)</a>
+    <a href="gawa/app-release.apk" download>Android APKをダウンロードする (app-release.apk)</a>
+    <a href="gawa/app-debug.apk" download>Android APKをダウンロードする (app-debug.apk)</a>
   </div>
 `
