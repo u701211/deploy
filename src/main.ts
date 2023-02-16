4274398a1cc17cd30c89e2ef66e1e4225ed1bf7b
takahiro tanaka <u701211.job@gmail.com>
2023-02-16 10:23:42 +0900

    commit

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,10 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h3>ガワアプリ</h3>
     <a href="download/gawa/app-release.apk" download>Android APKをダウンロードする (app-release.apk)</a> <br/>
-    <a href="download/gawa/app-debug.apk" download>Android APKをダウンロードする (app-debug.apk)</a>
     <br/>
     <br/>
     <br/>
     <h3>Flutterアプリテンプレート</h3>
     <a href="download/template/app-release.apk" download>Android APKをダウンロードする (app-release.apk)</a> <br/>
-    <a href="download/template/app-debug.apk" download>Android APKをダウンロードする (app-debug.apk)</a>
   </div>
 `
