32b6606f56b76958e44104066bb23149141d371c
Takahiro Tnaka <Takahiro.Tnaka.bp@jp.nttdata.com>
2023-02-20 09:21:16 +0900

    commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h3>ガワアプリ</h3>
     <a href="app-release.apk" download>リリース用 Android APKをダウンロードする (app-release.apk)</a> <br/>
-    <a href="app-debug.apk" download>デバッグ用 Android APKをダウンロードする (app-debug.apk)</a>
   </div>
 `
+
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//     <h3>ガワアプリ</h3>
+//     <a href="app-release.apk" download>リリース用 Android APKをダウンロードする (app-release.apk)</a> <br/>
+//     <a href="app-debug.apk" download>デバッグ用 Android APKをダウンロードする (app-debug.apk)</a>
+//   </div>
+// `
