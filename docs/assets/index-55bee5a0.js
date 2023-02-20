a38c6e8aa3126b06878feac783e2ba32904a02d8
takahiro tanaka <u701211.job@gmail.com>
2023-02-20 09:25:52 +0900

    commit

--- docs/assets/index-55bee5a0.js
+++
@@ -1,7 +0,0 @@
-(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const t of r.addedNodes)t.tagName==="LINK"&&t.rel==="modulepreload"&&n(t)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();document.querySelector("#app").innerHTML=`
-  <div>
-    <h3>ガワアプリ</h3>
-    <a href="app-release.apk" download>リリース用 Android APKをダウンロードする (app-release.apk)</a> <br/>
-    <a href="app-debug.apk" download>デバッグ用 Android APKをダウンロードする (app-debug.apk)</a>
-  </div>
-`;
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import './style.css'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h3>ガワアプリ</h3>
-    <a href="app-release.apk" download>リリース用 Android APKをダウンロードする (app-release.apk)</a> <br/>
+    <a href="app-release.apk" download>リリース用をダウンロード(app-release.apk)</a> <br/>
   </div>
 `
 
 // document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 //   <div>
 //     <h3>ガワアプリ</h3>
-//     <a href="app-release.apk" download>リリース用 Android APKをダウンロードする (app-release.apk)</a> <br/>
-//     <a href="app-debug.apk" download>デバッグ用 Android APKをダウンロードする (app-debug.apk)</a>
+    // <a href="app-release.apk" download>リリース用をダウンロード(app-release.apk)</a> <br/>
+    // <a href="app-debug.apk" download>デバッグ用をダウンロード(app-debug.apk)</a>
 //   </div>
 // `
